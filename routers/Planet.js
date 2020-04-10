ba288315def7073a0a8cd148f7fafab1770fcd82
Doheon Keum <heizence6626@gmail.com>
2020-04-10 18:30:39 +0900

    data retrieving function completed

--- routers/Planet.js
+++ routers/Planet.js
@@ -1,2 +1,37 @@
 const Router = require('router')
 const router = Router()
+const queries = require('../parseInfo')
+
+router.get('/planet', async (req, res) => {
+    queries.Planet.equalTo('name', 'Tatooine')
+    let result = await queries.Planet.find()
+    result = result[0]
+   
+    // why (key in result) doesn't work?
+    let temp = JSON.stringify(result)
+    temp = JSON.parse(temp)
+
+    for (let key in temp) {
+        if (temp[key]['__type']) {
+            console.log('relational object: ' , temp[key], 'key: ', key)
+            let className = temp[key]['className']
+
+            let relationalData = []
+
+            await result.relation(key).query().each(async object => {
+                queries[className].equalTo('objectId', object.id)
+                let output = await queries[className].find()
+                output = output[0]        
+                relationalData.push(output.get('name') || output.get('title'))
+            })
+            console.log(`relational Data(${className}) : `, relationalData)            
+            console.log('\n')
+            temp[key] = relationalData
+            console.log('modified temp data : ', temp)
+        }
+    }
+
+    res.send(temp)    
+})
+
+module.exports = router;

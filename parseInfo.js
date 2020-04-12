d904f6810b8d11b409e9754fa406f115ba73d68c
Doheon Keum <heizence6626@gmail.com>
2020-04-13 02:21:01 +0900

    changed into function form

--- parseInfo.js
+++ parseInfo.js
@@ -1,27 +1,37 @@
-const Parse = require('parse/node');
+/*
+Made this file as an operational function to make Parse Object refreshed when this function is executed in other modules.
 
-Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
-Parse.initialize(
-  'pnKgu5IKxHxRbqVt5L7l7bJLgvfE5GyLwUc7jzzr', // This is your Application ID
-  'Ky9Esl1I0NeUzo5QclWr2IcNAYM2ScrXy4bXzDny', // This is your Javascript key
-  'NklKwAxdk31VqATXhGDXycFl8ao8PIqL3i7Q9cRz' // This is your Master key (never use it in the frontend)
-);
+Actually there were malfunctions when exported Parse Object directly. 
+I guess those problems were due to conditional restrictions which still remains even after procedure finished. 
+By exporting function which contains all things necessary and make it executed whenever server gets request, 
+all problems solved.
+*/
 
-const planetParse = Parse.Object.extend('Planet')
-const characterParse = Parse.Object.extend('Character')
-const filmParse = Parse.Object.extend('Film')
-const specieParse = Parse.Object.extend('Specie')
-const starshipParse = Parse.Object.extend('Starship')
-const vehicleParse = Parse.Object.extend('Vehicle')
+module.exports = function() {
+  const Parse = require('parse/node');
 
-const Planet = new Parse.Query(planetParse);
-const Character = new Parse.Query(characterParse);
-const Film = new Parse.Query(filmParse);
-const Specie = new Parse.Query(specieParse);
-const Starship = new Parse.Query(starshipParse);
-const Vehicle = new Parse.Query(vehicleParse);
+  Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+  Parse.initialize(
+    'pnKgu5IKxHxRbqVt5L7l7bJLgvfE5GyLwUc7jzzr', // This is your Application ID
+    'Ky9Esl1I0NeUzo5QclWr2IcNAYM2ScrXy4bXzDny', // This is your Javascript key
+    'NklKwAxdk31VqATXhGDXycFl8ao8PIqL3i7Q9cRz' // This is your Master key (never use it in the frontend)
+  );
 
-module.exports = {
-  Planet, Character, Film, Specie, Starship, Vehicle
+  const planetParse = Parse.Object.extend('Planet')
+  const characterParse = Parse.Object.extend('Character')
+  const filmParse = Parse.Object.extend('Film')
+  const specieParse = Parse.Object.extend('Specie')
+  const starshipParse = Parse.Object.extend('Starship')
+  const vehicleParse = Parse.Object.extend('Vehicle')
+
+  const Planet = new Parse.Query(planetParse);
+  const Character = new Parse.Query(characterParse);
+  const Film = new Parse.Query(filmParse);
+  const Specie = new Parse.Query(specieParse);
+  const Starship = new Parse.Query(starshipParse);
+  const Vehicle = new Parse.Query(vehicleParse);
+
+  return {
+    Planet, Character, Film, Specie, Starship, Vehicle
+  }
 }
-

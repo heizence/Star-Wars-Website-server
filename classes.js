0d1b7014e8d0626be72333d2bad88e07c2e11ecb
Doheon Keum <heizence6626@gmail.com>
2020-04-15 16:10:53 +0900

    Parse Classes

---
+++ classes.js
@@ -0,0 +1,42 @@
+/*
+Made this file as an operational function to make Parse Object refreshed when this function is executed in other modules.
+
+Actually there were malfunctions when exported Parse Object directly. 
+I guess those problems were due to conditional restrictions which still remains even after procedure finished. 
+By exporting function which contains all things necessary and make it executed whenever server gets request, 
+all problems solved.
+*/
+
+const Parse = require('./parseInfomations');
+
+const dataClass = function() {
+  const planetParse = Parse.Object.extend('Planet')
+  const characterParse = Parse.Object.extend('Character')
+  const filmParse = Parse.Object.extend('Film')
+  const specieParse = Parse.Object.extend('Specie')
+  const starshipParse = Parse.Object.extend('Starship')
+  const vehicleParse = Parse.Object.extend('Vehicle')
+
+  const Planet = new Parse.Query(planetParse);
+  const Character = new Parse.Query(characterParse);
+  const Film = new Parse.Query(filmParse);
+  const Specie = new Parse.Query(specieParse);
+  const Starship = new Parse.Query(starshipParse);
+  const Vehicle = new Parse.Query(vehicleParse);
+
+  return {
+    Planet, Character, Film, Specie, Starship, Vehicle
+  }
+}
+
+const userClass = function() {
+  const CustomUserClass = Parse.Object.extend('CustomUserClass');
+  const user = new CustomUserClass();
+  const query = new Parse.Query(CustomUserClass);
+
+  return { user, query }
+}
+
+module.exports = {
+  dataClass, userClass
+}

f18e00be8987838cb57ef44d67806ca1ca71ea83
Doheon Keum <heizence6626@gmail.com>
2020-05-04 13:36:56 +0900

    final updates

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors');
 const corsOptions = {
-    origin: 'http://localhost:3000', 
     credentials: true
 };
 const bodyParser = require('body-parser')

b4f6342cca86f6a04baad3a3415efc93bf2bbc49
Doheon Keum <heizence6626@gmail.com>
2020-05-04 20:29:52 +0900

    updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const corsOptions = {
-    origin: 'https://starwarswebsitebydoheon.herokuapp.com',
+    origin: 'https://swapibydoheon.herokuapp.com',
     optionsSuccessStatus: 200
 }
 const bodyParser = require('body-parser')
